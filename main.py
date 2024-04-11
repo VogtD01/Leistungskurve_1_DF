fe1235da5721fd4b1a1fb45b8adb96e971925997
VogtD01 <vd6806@mci4me.at>
2024-04-11 16:17:07 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from load_data import load_data
+import matplotlib.pyplot as plt
+from sort import bubble_sort
+import numpy as np
 
 data=load_data('activity.csv')  
+
+power_original = data['PowerOriginal']
+power_sorted = bubble_sort(power_original)  # Sort the data
+
+plt.show()
+time = np.arange(0, len(power_original), 1)
+plt.plot(time/60, power_sorted)
+plt.xlabel('Time (minutes)')    
+plt.ylabel('Power (W)')
+
+
+plt.show()
+print(np.size(power_original))
 
