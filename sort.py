e35ac2316b417b1c74721ab73d5259257b7f74ae
VogtD01 <vd6806@mci4me.at>
2024-04-11 15:46:38 +0200

    update sort bug

--- sort.py
+++ sort.py
@@ -2,5 +2,5 @@ def bubble_sort(data):
     for number in range(len(data)-1):
         for number2 in range(len(data)-1):
             if data[number2] > data[number2+1]:
-                data[number2], data[number2+1] = data[number2+1], a[number2]
+                data[number2], data[number2+1] = data[number2+1], data[number2]
     return data
