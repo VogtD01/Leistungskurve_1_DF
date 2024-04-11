c2f4e4f36c681f05e55b23dcd6b004065dd29b22
felix3646 <felix.sturm2004@icloud.com>
2024-04-11 15:44:38 +0200

    added bubble sort

---
+++ sort.py
@@ -0,0 +1,6 @@
+def bubble_sort(data):
+    for number in range(len(data)-1):
+        for number2 in range(len(data)-1):
+            if data[number2] > data[number2+1]:
+                data[number2], data[number2+1] = data[number2+1], a[number2]
+    return data
