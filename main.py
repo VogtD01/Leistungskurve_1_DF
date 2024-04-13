63c029423970dbbade9682ff0b19fe0954992b8d
VogtD01 <vd6806@mci4me.at>
2024-04-13 08:06:59 +0200

    readme pull

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from load_data import load_data
 import matplotlib.pyplot as plt
 from sort import bubble_sort
 import numpy as np
+import os
 
 data=load_data('activity.csv')  
 
@@ -15,8 +16,15 @@ plt.plot(time/60, power_sorted)
 plt.xlabel("t / min")    
 plt.ylabel("P / W")
 plt.title("Power-Curve")
+
+# Prüfen, ob Ordner "figures" existiert sonst erstellen
+if not os.path.exists("figures"):
+    os.makedirs("figures")
+
+# Speichern des Plots im Ordner "figures"
+plt.savefig("figures/power_curve.png")
+
+# Anzeigen des Plots
 plt.show()
 
-#save plot
-plt.savefig("figures.png")
 
